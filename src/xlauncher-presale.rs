299e510b41e8fd928ecbf6eee5fdd52c1516e195
Bogdan <bogdan.oloeriu@gmail.com>
2022-03-19 15:45:48 +0100

    added fund test

--- src/xlauncher-presale.rs
+++ src/xlauncher-presale.rs
@@ -6,10 +6,31 @@ elrond_wasm::derive_imports!();
 #[elrond_wasm::derive::contract]
 pub trait XLauncherPresale {
     #[init]
-    fn init(&self, token_id: TokenIdentifier) {
+    fn init(&self,
+            token_id: TokenIdentifier) {
         require!(token_id.is_valid_esdt_identifier(), "Invalid token identifier");
         self.token_id().set(&token_id);
     }
+
+    #[endpoint(fundContract)]
+    #[payable("*")]
+    fn fund_contract(&self,
+                     #[payment_token] token_identifier: TokenIdentifier,
+                     #[payment] _payment: BigUint) {
+        require!(!self.token_id().is_empty(), "Token id not set");
+
+        let my_token_id = self.token_id().get();
+        require!( my_token_id == token_identifier, "Invalid fund token" )
+    }
+
+    #[view(getBalance)]
+    fn get_balance(&self) -> BigUint {
+        let my_token_id = self.token_id().get();
+        let balance: BigUint = self.blockchain().get_sc_balance(&my_token_id, 0);
+        return balance;
+    }
+
+    // storage
 
     #[view(getTokenId)]
     #[storage_mapper("tokenId")]
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -7,6 +7,8 @@
 elrond_wasm_node::wasm_endpoints! {
     xlauncher_presale
     (
+        fundContract
+        getBalance
         getTokenId
     )
 }
