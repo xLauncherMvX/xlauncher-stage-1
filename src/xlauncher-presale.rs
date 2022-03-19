8a6119d0eba8db585b76782cd0928e37da9dc6b7
Bogdan <bogdan.oloeriu@gmail.com>
2022-03-19 10:54:59 +0100

    added the wallets

--- src/xlauncher-presale.rs
+++ src/xlauncher-presale.rs
@@ -1,11 +1,19 @@
 #![no_std]
 
 elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
 
 #[elrond_wasm::derive::contract]
 pub trait XLauncherPresale {
     #[init]
     fn init(&self) {
         // no code required for now
+    }
+
+    fn set_token_identifier(
+        &self,
+        _token_identifier: ManagedBuffer,
+    ) -> SCResult<()> {
+        Ok(())
     }
 }
