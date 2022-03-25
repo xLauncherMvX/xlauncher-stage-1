3d21b0b39bca938bcce31dd1fc88d40c233eddad
Bogdan <bogdan.oloeriu@gmail.com>
2022-03-25 14:16:42 +0100

    added price to tests

--- src/xlauncher-presale.rs
+++ src/xlauncher-presale.rs
@@ -7,9 +7,12 @@ elrond_wasm::derive_imports!();
 pub trait XLauncherPresale {
     #[init]
     fn init(&self,
-            token_id: TokenIdentifier) {
+            token_id: TokenIdentifier,
+            initial_price: BigUint) {
         require!(token_id.is_valid_esdt_identifier(), "Invalid token identifier");
+        require!(initial_price > 0, "Initial price must be positive value");
         self.token_id().set(&token_id);
+        self.price().set(initial_price);
     }
 
     #[endpoint(fundContract)]
@@ -18,7 +21,6 @@ pub trait XLauncherPresale {
                      #[payment_token] token_identifier: TokenIdentifier,
                      #[payment] _payment: BigUint) {
         require!(!self.token_id().is_empty(), "Token id not set");
-
         let my_token_id = self.token_id().get();
         require!( my_token_id == token_identifier, "Invalid fund token" )
     }
@@ -31,8 +33,11 @@ pub trait XLauncherPresale {
     }
 
     // storage
-
     #[view(getTokenId)]
     #[storage_mapper("tokenId")]
     fn token_id(&self) -> SingleValueMapper<TokenIdentifier>;
+
+    #[view(getPrice)]
+    #[storage_mapper("price")]
+    fn price(&self) -> SingleValueMapper<BigUint>;
 }
