ae0b371716ff76bc298c98f105d8337a4dc4c503
Bogdan <bogdan.oloeriu@gmail.com>
2022-03-26 18:42:15 +0100

    switch to xlh

--- interaction/devnet.snippets.sh
+++ interaction/devnet.snippets.sh
@@ -11,8 +11,8 @@ CHAINID=D
 MY_LOGS="interaction-logs"
 ENV_LOGS="devnet"
 
-#TOKEN_ID="XLH-06bd23"
-TOKEN_ID="BCOIN-aafe6c"
+TOKEN_ID="XLH-06bd23"
+#TOKEN_ID="BCOIN-aafe6c"
 TOKEN_ID_HEX=$(echo -n ${TOKEN_ID} | xxd -p)
 
 deploy() {
