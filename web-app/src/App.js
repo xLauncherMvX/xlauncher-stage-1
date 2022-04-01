dd323dc8e2436e1f1603e3bcf07d460a9ed51cd7
Bogdan <bogdan.oloeriu@gmail.com>
2022-04-01 23:02:59 +0200

    wired elrond provider

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -10,19 +10,28 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-import theme from "./theme/defaultDark";
+import theme from './theme/defaultDark';
 import ScratchBare from './components/scratchBare';
 import { Outlet } from 'react-router-dom';
+import { DappProvider } from '@elrondnetwork/dapp-core';
+
+const environment = 'devnet';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-        <Grid >
-            <ScratchBare/>
-          <VStack >
-          <Outlet/>
+      <DappProvider
+        environment={environment}
+        customNetworkConfig={{ name: 'customConfig', apiTimeout: 6000 }}
+        completedTransactionsDelay={200}
+      >
+        <Grid>
+          <ScratchBare />
+          <VStack>
+            <Outlet />
           </VStack>
         </Grid>
+      </DappProvider>
     </ChakraProvider>
   );
 }
