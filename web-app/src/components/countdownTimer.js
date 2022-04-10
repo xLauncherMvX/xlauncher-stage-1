c6e789b5ada846e962164a54506d45978aed90f8
IonutCioarec <ionutcioarec93@gmail.com>
2022-04-10 13:01:06 +0300

    Added Grid System to countdown timer section

--- web-app/src/components/countdownTimer.js
+++ web-app/src/components/countdownTimer.js
@@ -1,17 +1,25 @@
 import DateCountdown from "./hooks/dateCountdown";
-import { Progress, Text } from '@chakra-ui/react';
+import {
+    Box,
+    Container,
+    Grid,
+    GridItem,
+    Progress, 
+    Text
+  } from '@chakra-ui/react';
 
 export default function CountdownTimer(){
     // Render a countdown
     return (
-        <div className="show-counter" >
-            <p className='seedsale-text' align={'center'}>Seed Sale starts in:</p>
-            <a className="countdown-link">
-                <DateCountdown dateTo='April 17, 2022 00:00:00 GMT+03:00'/>
-            </a>   
-            <Progress hasStripe value={50} height='32px' colorScheme='blue' marginTop={'4'}/>
-            <Text align={'center'} fontSize={'20'} fontWeight={'bold'} mt={'2'}> 0 / 13000000 XLH</Text>
-        </div>
-        
+        <Box as={Container} maxW="7xl" mt={14}>        
+            <div className="show-counter" >
+                <p className='seedsale-text' align={'center'}>Seed Sale starts in:</p>
+                <a className="countdown-link">
+                    <DateCountdown dateTo='April 17, 2022 00:00:00 GMT+03:00'/>
+                </a>   
+                <Progress hasStripe value={50} height='32px' colorScheme='blue' marginTop={'4'}/>
+                <Text align={'center'} fontSize={'20'} fontWeight={'bold'} mt={'2'}> 0 / 13000000 XLH</Text>
+            </div>
+    </Box>
     );
 }
