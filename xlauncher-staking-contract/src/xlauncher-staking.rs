d45889fbe45bbe24a5148d3bff09aaea05e99282
Bogdan <bogdan.oloeriu@gmail.com>
2022-04-18 17:08:07 +0200

    added 3 params to init

--- xlauncher-staking-contract/src/xlauncher-staking.rs
+++ xlauncher-staking-contract/src/xlauncher-staking.rs
@@ -4,9 +4,22 @@ elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
 #[elrond_wasm::derive::contract]
-pub trait XLauncherStaking{
+pub trait XLauncherStaking {
     #[init]
-    fn init(&self) {
-
+    fn init(&self,
+            token_id: TokenIdentifier,
+            min_amount: BigUint,
+            locking_time_span_a: u64,
+    ) {
+        require!(token_id.is_valid_esdt_identifier(), "invalid token_id");
+        require!(min_amount > 0, "min_amount must be positive");
+        require!(0u64 <= locking_time_span_a, "locking_time_span_a to small");
+        self.token_id().set(&token_id);
     }
+
+    // storage
+
+    #[view(getTokenId)]
+    #[storage_mapper("tokenId")]
+    fn token_id(&self) -> SingleValueMapper<TokenIdentifier>;
 }
