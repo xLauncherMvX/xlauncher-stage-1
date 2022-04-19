cac470333b9270e474a42dc42b630ad375211ca6
Bogdan <bogdan.oloeriu@gmail.com>
2022-04-19 16:57:32 +0200

    managed to persist and run tests

--- xlauncher-staking-contract/src/xlauncher-staking.rs
+++ xlauncher-staking-contract/src/xlauncher-staking.rs
@@ -62,20 +62,22 @@ pub trait XLauncherStaking {
         let client = self.blockchain().get_caller();
         let current_time_stamp = self.blockchain().get_block_timestamp();
         let mut stateVector = self.client_state(&client);
-        let new_pull_state = ClientPullState {
-            pull_id: (pull_id),
-            pull_time_stamp_entry: (current_time_stamp),
-            pull_time_stamp_last_collection: (current_time_stamp),
-            pull_amount: amount,
-        };
+
 
         if stateVector.is_empty() {
+            let new_pull_state = ClientPullState {
+                pull_id: (pull_id),
+                pull_time_stamp_entry: (current_time_stamp),
+                pull_time_stamp_last_collection: (current_time_stamp),
+                pull_amount: amount,
+            };
             stateVector.push(&new_pull_state);
         } else {
             for i in 1..=stateVector.len() {
                 let mut prev_pull_state = stateVector.get(i);
                 if prev_pull_state.pull_id == pull_id {
-                    let rewords = self.calculate_rewords(prev_pull_state);
+                    let rewords = self.calculate_rewords(
+                        prev_pull_state.pull_amount.clone());
                     prev_pull_state.pull_amount += (&rewords + &amount);
                     prev_pull_state.pull_time_stamp_entry = current_time_stamp;
                     prev_pull_state.pull_time_stamp_last_collection = current_time_stamp;
@@ -87,8 +89,8 @@ pub trait XLauncherStaking {
     }
 
     fn calculate_rewords(&self,
-                         pull_state: ClientPullState<Self::Api>) -> BigUint {
-        pull_state.pull_amount / 2u32
+                         pull_amount: BigUint<Self::Api>) -> BigUint {
+        pull_amount / 2u32
     }
 
     fn is_valid_pull_id(&self,
