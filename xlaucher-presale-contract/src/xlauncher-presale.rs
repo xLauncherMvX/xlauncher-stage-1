713834495e368876e2206cc7b62df0b7d4d8211f
Sorin Petreasca <petreasca.sorin@gmail.com>
2022-05-22 11:59:06 +0300

    Presale contract notes

--- xlaucher-presale-contract/src/xlauncher-presale.rs
+++ xlaucher-presale-contract/src/xlauncher-presale.rs
@@ -9,17 +9,25 @@ const ZERO: u64 = 0;
 #[elrond_wasm::derive::contract]
 pub trait XLauncherPresale {
     #[init]
-    fn init(&self,
-            token_id: TokenIdentifier,
-            initial_price: BigUint,
-            min_amount: BigUint,
-            max_amount_val: BigUint,
-            max_balance_val: BigUint) {
-        require!(token_id.is_valid_esdt_identifier(), "Invalid token identifier");
+    fn init(
+        &self,
+        token_id: TokenIdentifier,
+        initial_price: BigUint,
+        min_amount: BigUint,
+        max_amount_val: BigUint,
+        max_balance_val: BigUint,
+    ) {
+        require!(
+            token_id.is_valid_esdt_identifier(),
+            "Invalid token identifier"
+        );
         require!(initial_price > 0, "Initial price must be positive value");
         require!(min_amount > 0, "Min amount must be positive value");
         require!(max_amount_val > 0, "Max amount must be positive value");
-        require!(min_amount < max_amount_val, "min amount must be smaller then max amount");
+        require!(
+            min_amount < max_amount_val,
+            "min amount must be smaller then max amount"
+        );
         require!(max_balance_val > 0, "Max balance must be positive value");
         self.token_id().set(&token_id);
         self.price().set(initial_price);
@@ -28,16 +36,33 @@ pub trait XLauncherPresale {
         self.max_balance().set(max_balance_val);
     }
 
+    // I recommend to format the code before each commit (if you use VSCode) 
+    // To be easier to read and most importantly for comparing future commits
+
+    // NOTE - for consistency, try to use this particular order:
+    // #[only_owner]
+    // #[payable("*")]
+    // #[endpoint(fundContract)]
+
+    // NOTE
+    // You only check for the token identifier, so there is no need to use annotations, as they are not recommended anymore
+    // You can use the Call Value API for the token identifier only -> self.call_value().token()
     #[endpoint(fundContract)]
     #[payable("*")]
-    fn fund_contract(&self,
-                     #[payment_token] token_identifier: TokenIdentifier,
-                     #[payment] _payment: BigUint) {
+    fn fund_contract(
+        &self,
+        #[payment_token] token_identifier: TokenIdentifier,
+        #[payment] _payment: BigUint,
+    ) {
         require!(!self.token_id().is_empty(), "Token id not set");
         let my_token_id = self.token_id().get();
-        require!( my_token_id == token_identifier, "Invalid fund token" )
+        require!(my_token_id == token_identifier, "Invalid fund token")
     }
 
+    // NOTE
+    // Why rely on the balance of the SC and not save the remaining amount in a storage (also as a view)?
+    // If in the future you decide to use those tokens in other places as well, it may be difficult to migrate the logic
+    // You could use the self.call_value().payment_token_pair() call in the fund_contract method to check the token_id and to store the amount
     #[view(getTokenBalance)]
     fn get_token_balance(&self) -> BigUint {
         let my_token_id = self.token_id().get();
@@ -45,32 +70,33 @@ pub trait XLauncherPresale {
         return balance;
     }
 
-
+    // NOTE
+    // From what I see, max_balance() represents the total number of tokens an address can buy
+    // If you don't save the amount of tokens already bought by each address, this can be easily exploitable
+    // If you check only for the current_balance, the user can buy tokens, send them to another address and buy again the max amount
+    // Also, this will not work as intended, as get_esdt_token_data() only works for addresses that are in the same shard as the smart contract
     #[payable("EGLD")]
     #[endpoint]
-    fn buy(
-        &self
-    ) {
+    fn buy(&self) {
         let (payment_amount, payment_token) = self.call_value().payment_token_pair();
-        require!(payment_token.is_egld(),"Only EGLD");
+        require!(payment_token.is_egld(), "Only EGLD");
         require!(
             payment_amount >= self.min_amount().get(),
-            "Payment amount is to low"
+            "Payment amount is too low"
         );
         require!(
             payment_amount <= self.max_amount().get(),
-            "Payment amount is to high"
+            "Payment amount is too high"
         );
         let balance = self.get_token_balance();
-        require!(
-            balance > ZERO,
-            "No more tokens to sale."
-        );
+        require!(balance > ZERO, "No more tokens to sale.");
         let current_price = self.price().get();
         let one_egld = BigUint::from(EGLD_DECIMALS_VALUE);
-        let result_edst_token_amount = (&current_price * &payment_amount) / one_egld;
+        let result_esdt_token_amount = (&current_price * &payment_amount) / one_egld;
+        
+        // Maybe balance >= result_esdt_token_amount to cover all available ESDT amount
         require!(
-            balance > result_edst_token_amount,
+            balance > result_esdt_token_amount,
             "Not enough tokens for sale."
         );
 
@@ -78,28 +104,31 @@ pub trait XLauncherPresale {
         let caller = self.blockchain().get_caller();
         let token_id_val = self.token_id().get();
 
-        let client_token_balance = self.blockchain().get_esdt_token_data(
-            &caller, &token_id_val, 0).amount;
-        let client_total_balance = (&result_edst_token_amount + &client_token_balance);
+        let client_token_balance = self
+            .blockchain()
+            .get_esdt_token_data(&caller, &token_id_val, 0)
+            .amount;
+        let client_total_balance = (&result_esdt_token_amount + &client_token_balance);
         let max_balance_val = self.max_balance().get();
-        require!(client_total_balance <= max_balance_val,
-         "Buying that much will exceed max allowed token balance");
+        require!(
+            client_total_balance <= max_balance_val,
+            "Buying that much will exceed max allowed token balance"
+        );
 
-
-        self.send().direct(
-            &caller,
-            &token_id_val,
-            0, &result_edst_token_amount,
-            &[]);
+        self.send()
+            .direct(&caller, &token_id_val, 0, &result_esdt_token_amount, &[]);
         /* let owner = self.blockchain().get_owner_address();
-         self.send().direct_egld(
-             &owner,
-             &payment_amount,
-             &[],
-         )*/
+        self.send().direct_egld(
+            &owner,
+            &payment_amount,
+            &[],
+        )*/
     }
 
-
+    // NOTE
+    // No need for get_balance() method as you can use get_sc_balance for EGLD as well
+    // From Docs: For fungible ESDT, nonce should be 0. To get the EGLD balance, you can simply pass TokenIdentifier::egld() as parameter
+    // For flexibility, I would recommend 2 different methods for each token (collect_esdt & collect_egld)
     #[only_owner]
     #[endpoint]
     fn collect(&self) {
@@ -113,23 +142,20 @@ pub trait XLauncherPresale {
 
         let big_zero: BigUint = BigUint::from(0u32);
         if big_zero < token_balance {
-            self.send().direct(
-                &owner,
-                &my_token_id,
-                0, &token_balance,
-                &[]);
+            self.send()
+                .direct(&owner, &my_token_id, 0, &token_balance, &[]);
         }
 
         if big_zero < egld_balance {
-            self.send().direct_egld(
-                &owner,
-                &egld_balance,
-                &[])
+            self.send().direct_egld(&owner, &egld_balance, &[])
         }
     }
 
-
     // storage
+
+    // NOTE
+    // For our case this type of storage is simple and good enough
+    // But I recommend you to read about FungibleTokenMapper and NonFungibleTokenMapper for future contracts
     #[view(getTokenId)]
     #[storage_mapper("tokenId")]
     fn token_id(&self) -> SingleValueMapper<TokenIdentifier>;
