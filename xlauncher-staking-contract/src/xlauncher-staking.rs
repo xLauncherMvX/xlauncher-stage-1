2bede736547042ee6b69c65e5e8099ffce819d70
Bogdan <bogdan.oloeriu@gmail.com>
2022-06-04 13:20:24 +0200

    removed debug line

--- xlauncher-staking-contract/src/xlauncher-staking.rs
+++ xlauncher-staking-contract/src/xlauncher-staking.rs
@@ -899,7 +899,6 @@ pub trait XLauncherStaking {
                             &current_time_stamp,
                         );
                         rep_item.rewards_amount = rep_item.rewards_amount.clone() + item_reward;
-                        sc_print!("pool_id={}, rewords_amount={}",client_item.pool_id.clone(), rep_item.rewards_amount.clone())
                     }
                 }
             }
