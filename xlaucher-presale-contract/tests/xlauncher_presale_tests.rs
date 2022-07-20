e71f99ce78b011629acff9405cbde6042582858e
Bogdan <bogdan.oloeriu@gmail.com>
2022-07-20 12:43:29 +0300

    disabled the other tests

--- xlaucher-presale-contract/tests/xlauncher_presale_tests.rs
+++ xlaucher-presale-contract/tests/xlauncher_presale_tests.rs
@@ -19,15 +19,15 @@ fn world() -> BlockchainMock {
 
 #[test]
 fn buy_ok_scen_01() {
-    elrond_wasm_debug::mandos_rs("mandos/03-buy-ok.scen.json", world());
+    elrond_wasm_debug::mandos_rs("../mandos/03-buy-ok.scen", world());
 }
 
 #[test]
 fn test_pricing_for_presale_round_2() {
-    elrond_wasm_debug::mandos_rs("mandos/08-test-pricing-for-presale-round-2.scen.json", world());
+    elrond_wasm_debug::mandos_rs("../mandos/08-test-pricing-for-presale-round-2.scen", world());
 }
 
 #[test]
 fn test_stake_and_buy_ok() {
-    elrond_wasm_debug::mandos_rs("mandos/09-stake-and-buy-ok.scen.json", world());
+    elrond_wasm_debug::mandos_rs("../mandos/09-stake-and-buy-ok.json", world());
 }
