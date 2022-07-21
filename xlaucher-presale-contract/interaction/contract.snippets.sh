e9c76a53609699e3223e4065fa9b35ea574fd760
Bogdan <bogdan.oloeriu@gmail.com>
2022-07-21 09:33:43 +0300

    fixed coeficients

--- xlaucher-presale-contract/interaction/contract.snippets.sh
+++ xlaucher-presale-contract/interaction/contract.snippets.sh
@@ -24,14 +24,15 @@ MY_LOGS="interaction-logs"
 #token id values: devnet=XLH-4f55ab, testnet=XLH-0be7d1, mainnet=XLH-8daa50
 
 INITIAL_PRICE="6500${MY_DECIMALS}"
-MIN_AMOUNT="6500${MY_DECIMALS}"
-MAX_AMOUNT="32500${MY_DECIMALS}"
+MIN_AMOUNT="1${MY_DECIMALS}"
+MAX_AMOUNT="5${MY_DECIMALS}"
 MAX_BALANCE="32500${MY_DECIMALS}"
 
 setEnvDevnet() {
   cp -f erdpy.data-storage-devnet.json erdpy.data-storage.json
   CURRENT_ENV="devnet"
-  PEM_FILE="${PROJECT}/../../wallets/users/devnet_owner_wallet.pem"
+#  PEM_FILE="${PROJECT}/../../wallets/users/devnet_owner_wallet.pem"
+  PEM_FILE="${PROJECT}/../../wallets/users/client1.pem"
   ADDRESS=$(erdpy data load --key=address-devnet)
   PROXY=https://devnet-gateway.elrond.com
   CHAINID=D
@@ -43,7 +44,8 @@ setEnvDevnet() {
 setEnvTestnet() {
   cp -f erdpy.data-storage-testnet.json erdpy.data-storage.json
   CURRENT_ENV="testnet"
-  PEM_FILE="${PROJECT}/../../wallets/users/testnet_owner_wallet.pem"
+#  PEM_FILE="${PROJECT}/../../wallets/users/testnet_owner_wallet.pem"
+  PEM_FILE="${PROJECT}/../../wallets/users/client1.pem"
   ADDRESS=$(erdpy data load --key=address-devnet)
   PROXY=https://testnet-gateway.elrond.com
   CHAINID=T
@@ -134,9 +136,9 @@ getMaxBalance() {
 buyTokens() {
   erdpy --verbose contract call ${ADDRESS} --recall-nonce \
     --pem=${PEM_FILE} \
-    --gas-limit=3000000 \
+    --gas-limit=4000000 \
     --function="buy" \
-    --value=1000000000000000000 \
+    --value="4${MY_DECIMALS}" \
     --proxy=${PROXY} --chain=${CHAINID} \
     --send \
     --outfile="${MY_LOGS}/buyTokens-${ENV_LOGS}.json"
