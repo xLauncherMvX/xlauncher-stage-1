86615f6995bbceb1e6ba3170d64dd465053feb1e
Bogdan <bogdan.oloeriu@gmail.com>
2022-08-05 17:41:28 +0200

    changed back the funding sum

--- xlaucher-presale-contract/interaction/contract.snippets.sh
+++ xlaucher-presale-contract/interaction/contract.snippets.sh
@@ -103,7 +103,7 @@ updateContract() {
 fundContract() {
   method_name="0x$(echo -n 'fundContract' | xxd -p -u | tr -d '\n')"
   token_id="0x$(echo -n ${TOKEN_ID} | xxd -p -u | tr -d '\n')"
-  #  amount="20000001${MY_DECIMALS}"
+  #  amount="2000001${MY_DECIMALS}"
   amount="2000001${MY_DECIMALS}"
   erdpy --verbose contract call ${ADDRESS} --recall-nonce \
     --pem=${PEM_FILE} \
