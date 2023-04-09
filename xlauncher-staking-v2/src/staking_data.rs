2939a32ce68d37c337817a08aaa4f6bd0825bb74
Bogdan <bogdan.oloeriu@gmail.com>
2023-04-09 13:28:10 +0200

    aded sft data model

--- xlauncher-staking-v2/src/staking_data.rs
+++ xlauncher-staking-v2/src/staking_data.rs
@@ -1,6 +1,6 @@
 use multiversx_sc::{
     api::ManagedTypeApi,
-    types::{BigUint, TokenIdentifier},
+    types::{BigUint, TokenIdentifier, ManagedAddress, ManagedVec},
 };
 
 
@@ -11,5 +11,34 @@ pub struct StakingSettings<M: ManagedTypeApi> {
     pub token_id: TokenIdentifier<M>,
     pub min_staking_val: BigUint<M>,
     pub max_staking_val: BigUint<M>,
+    pub total_staked_value: BigUint<M>,
+    pub total_available_for_rewords: BigUint<M>,
+    pub unstake_xlh_lock_span: u64,
+    pub unstake_sft_lock_span: u64,
+    pub last_pool_id: usize, // if id is 0, then there are no pools
+}
+
+pub struct PoolData<M:ManagedTypeApi>{
+    pub pool_id: usize,
+    pub pool_total_staked: BigUint<M>,
+}
+
+
+pub struct ClientXlhData<M:ManagedTypeApi>{
+    pub pull_id: usize,
+    pub client_total_staked: BigUint<M>,
+}
+
+pub struct ClientXlhUnstakeData<M:ManagedTypeApi>{
+    pub total_unstake_amount: BigUint<M>,
+    pub requested_amount: BigUint<M>,
+    pub requested_time_stamp: u64,
+    pub free_after_time_stamp: u64,
+}
+
+pub struct ClientSftUnstakeData<M:ManagedTypeApi>{
+    pub unstake_amount: BigUint<M>,
+    pub requested_time_stamp: u64,
+    pub free_after_time_stamp: u64,
 }
 
--- xlauncher-staking-v2/src/xlauncher_staking.rs
+++ xlauncher-staking-v2/src/xlauncher_staking.rs
@@ -15,11 +15,22 @@ pub trait HelloWorld {
 
     #[only_owner]
     #[endpoint(setContractSettings)]
-    fn set_contract_settings(&self, token_id: TokenIdentifier, min_staking_val: BigUint, max_staking_val: BigUint) {
+    fn set_contract_settings(&self,
+                             token_id: TokenIdentifier,
+                             min_staking_val: BigUint,
+                             max_staking_val: BigUint,
+                             unstake_xlh_lock_span: u64,
+                             unstake_sft_lock_span: u64,
+                             ) {
         let settings = StakingSettings {
             token_id,
             min_staking_val,
             max_staking_val,
+            unstake_xlh_lock_span,
+            unstake_sft_lock_span,
+            total_staked_value: BigUint::zero(),
+            total_available_for_rewords: BigUint::zero(),
+            last_pool_id: 0,
         };
         self.contract_settings().set(&settings);
     }
