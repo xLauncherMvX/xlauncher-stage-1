ef5abe2edeab7943b01378cf3fc220fbdca5ee36
Bogdan <bogdan.oloeriu@gmail.com>
2023-04-16 19:43:17 +0200

    set contract settins

--- xlauncher-staking-v2/interaction/contract-snippets.sh
+++ xlauncher-staking-v2/interaction/contract-snippets.sh
@@ -1,18 +1,30 @@
 PROJECT="${PWD}"
 DEPLOY_TRANSACTION=$(mxpy data load --key=deployTransaction-devnet)
 CORE_LOGS="interaction-logs"
+MY_DECIMALS="000000000000000000"
+
 setEnvDevnet(){
   CURRENT_ENV="devnet"
+  ENV_LOGS="${CORE_LOGS}/${CURRENT_ENV}"
+
   cp -f mxpy.data-storage-devnet.json mxpy.data-storage.json
   PEM_FILE="${PROJECT}/../../wallets/users/devnet_owner_wallet.pem"
   ADDRESS=$(mxpy data load --key=address-devnet)
   PROXY=https://devnet-gateway.multiversx.com
   CHAINID=D
+
   TOKEN_ID="XLH-4a7cc0"
-  TOKEN_ID_HEX=$(echo -n ${TOKEN_ID} | xxd -p)
   SFT_ID="SFT-8ff335"
+
+  TOKEN_ID_HEX=$(echo -n ${TOKEN_ID} | xxd -p)
   SFT_ID_HEX=$(echo -n ${SFT_ID} | xxd -p)
-  ENV_LOGS="${CORE_LOGS}/${CURRENT_ENV}"
+
+  MAX_STAKING_VAL="1000000${MY_DECIMALS}"
+  UNSTAKE_XLH_LOCK_SPAN="864000"
+  UNSTAKE_SFT_LOCK_SPAN="5184000"
+  MIN_APY="150000"
+  MAX_APY="300000"
+  SFT_INCREMENT_APY="15000"
 }
 
 deploy() {
@@ -31,7 +43,17 @@ deploy() {
   echo "Smart contract address: ${ADDRESS}"
 }
 
-setSettings(){
-
+setContractSettings(){
+  MY_LOGS="${ENV_LOGS}-setContractSettings.json"
+    mxpy --verbose contract call ${ADDRESS} --recall-nonce \
+      --pem=${PEM_FILE} \
+      --gas-limit=8000000 \
+      --proxy=${PROXY} --chain=${CHAINID} \
+      --function="setContractSettings" \
+      --arguments "0x${TOKEN_ID_HEX}" "0x${SFT_ID_HEX}" 1 \
+      ${MAX_STAKING_VAL} ${UNSTAKE_XLH_LOCK_SPAN} ${UNSTAKE_SFT_LOCK_SPAN} \
+      ${MIN_APY} ${MAX_APY} ${SFT_INCREMENT_APY} \
+      --send \
+      --outfile="${MY_LOGS}"
 }
 
