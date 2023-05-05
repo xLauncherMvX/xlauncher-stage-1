6db1b4ba0cbdfa570fcd5859472582d6b56f357e
Bogdan <bogdan.oloeriu@gmail.com>
2023-05-05 23:07:49 +0200

    we need to fix pool data

--- xlauncher-staking-v2/tests/xlauncher_rs_test.rs
+++ xlauncher-staking-v2/tests/xlauncher_rs_test.rs
@@ -16,22 +16,22 @@ fn world() -> ScenarioWorld {
 
 #[test]
 fn _03_create_new_pool() {
-    multiversx_sc_scenario::run_rs("scenarios/_03_create_new_pool.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive1/_03_create_new_pool.scen.json", world());
 }
 
 #[test]
 fn fund_with_rewords_rs() {
-    multiversx_sc_scenario::run_rs("scenarios/03-fund-with-rewords.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/03-fund-with-rewords.scen.json", world());
 }
 
 #[test]
 fn _04_stake_xlh_1_client() {
-    multiversx_sc_scenario::run_rs("scenarios/04-stake-xlh-1-client.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/04-stake-xlh-1-client.scen.json", world());
 }
 
 #[test]
 fn claim_1_year_rs() {
-    multiversx_sc_scenario::run_rs("scenarios/05-claim-1-year.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/05-claim-1-year.scen.json", world());
 }
 
 #[test]
@@ -41,20 +41,20 @@ fn stake_sft_rs() {
 
 #[test]
 fn claim_1_year_10_sft_rs() {
-    multiversx_sc_scenario::run_rs("scenarios/06-claim-1-year-10-sft.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/06-claim-1-year-10-sft.scen.json", world());
 }
 
 #[test]
 fn top_up_1_year_rs() {
-    multiversx_sc_scenario::run_rs("scenarios/07-top-up-1-year.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/07-top-up-1-year.scen.json", world());
 }
 
 #[test]
 fn _08_unstake_half_1_year_rs() {
-    multiversx_sc_scenario::run_rs("scenarios/08-unstake-half-1-year.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/08-unstake-half-1-year.scen.json", world());
 }
 
 #[test]
 fn _08_unstake_all_1_year() {
-    multiversx_sc_scenario::run_rs("scenarios/08-unstake-all-1-year.scen.json", world());
+    multiversx_sc_scenario::run_rs("../scenarios/archive2/08-unstake-all-1-year.scen.json", world());
 }
